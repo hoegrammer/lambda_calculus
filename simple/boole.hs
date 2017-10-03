ff2ea1b6487e25241cc8e45ef97b01ab0d01c971
naomi <naomi@aktivix.org>
2017-10-03 19:32:56 +0300

    Added xor, simplified function definitions

--- simple/boole.hs
+++ simple/boole.hs
@@ -6,14 +6,17 @@ false x y = y
 true :: a -> a -> a
 true x y = x
 
-c_not :: ((a -> a -> a) -> (a -> a -> a) -> (a -> a -> a)) -> a -> a -> a
-c_not x = x false true
+c_not :: (a -> a -> a)  -> (a -> a -> a)
+c_not x y z = x z y
 
-c_or :: ((a -> a -> a) -> (a -> a -> a) -> (a -> a -> a)) -> ((a -> a -> a) -> (a -> a -> a) -> (a -> a -> a)) -> a -> a -> a 
-c_or x y =  x true (y true false)
+c_or :: ((a -> a -> a) -> (a -> a -> a)  ->  (a -> a -> a))  -> (a -> a -> a) -> (a -> a -> a)
+c_or x y =  x true y
 
-c_and :: ((a -> a -> a) -> (a -> a -> a) -> (a -> a -> a)) -> ((a -> a -> a) -> (a -> a -> a) -> (a -> a -> a)) -> a -> a -> a 
-c_and x y = x (y true false) false
+c_and :: ((a -> a -> a) -> (a -> a -> a)  ->  (a -> a -> a))  -> (a -> a -> a) -> (a -> a -> a)
+c_and x y = x y false
+
+c_xor :: ((a -> a -> a) -> (a -> a -> a)  ->  (a -> a -> a))  -> (a -> a -> a) -> (a -> a -> a)
+c_xor x y = x (c_not y) y
 
 if_then_else = id  -- if_then_else test if_thing else_thing = test if_thing else_thing
 
@@ -43,6 +46,11 @@ testc = TestCase (assertEqual "false and true is false" (False) (church_to_boole
 testd = TestCase (assertEqual "if_then_else returns if_thing when test is true" (True) (church_to_boolean (if_then_else true true false)))
 teste = TestCase (assertEqual "if_then_else return else_thing when test is false" (False) (church_to_boolean (if_then_else false true false)))
 
+testf = TestCase (assertEqual "true xor true is false" (False) (church_to_boolean (c_xor true true)))
+testg = TestCase (assertEqual "true xor false is True" (True) (church_to_boolean (c_xor true false)))
+testh = TestCase (assertEqual "false xor false is false" (False) (church_to_boolean (c_xor false false)))
+testi = TestCase (assertEqual "false xor true is false" (True) (church_to_boolean (c_xor false true)))
+
 tests = TestList [
   TestLabel "test1" test1 , 
   TestLabel "test2" test2 , 
@@ -57,4 +65,8 @@ tests = TestList [
   TestLabel "testb" testb,
   TestLabel "testc" testc,
   TestLabel "testd" testd,
-  TestLabel "teste" teste ]
+  TestLabel "teste" teste,
+  TestLabel "testf" testf,
+  TestLabel "testg" testg,
+  TestLabel "testh" testh,
+  TestLabel "testi" testi ]
